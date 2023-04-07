2efc56a2f556a770f07fdd272b768cce796f0c38
setokk <tesmtracks@gmail.com>
2023-04-07 18:31:26 +0300

    Initial Commit. Added Navigation Editor and Safe-Args Dependencies

---
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.Navigation;
+
+public class LoginActivity extends AppCompatActivity
+{
+    @Override
+    public void onCreate(Bundle savedInstanceBundle)
+    {
+        super.onCreate(savedInstanceBundle);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/PsfHomeFragment.java
@@ -0,0 +1,88 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.myapplication.databinding.FragmentPsfDoctorBinding;
+import com.example.myapplication.databinding.FragmentPsfHomeBinding;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link PsfHomeFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class PsfHomeFragment extends Fragment
+{
+    private FragmentPsfHomeBinding binding;
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public PsfHomeFragment()
+    {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment HomeFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static PsfHomeFragment newInstance(String param1, String param2)
+    {
+        PsfHomeFragment fragment = new PsfHomeFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        binding = FragmentPsfHomeBinding.inflate(getLayoutInflater());
+
+        if (getArguments() != null)
+        {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState)
+    {
+        binding = FragmentPsfHomeBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState)
+    {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.psfHomeButton.setOnClickListener(v ->
+                Navigation.findNavController(view).navigate(R.id.action_psfHomeFragment_to_psfDoctorFragment));
+    }
+}
