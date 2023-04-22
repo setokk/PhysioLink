51a4e12b6540775b5859e8754b724c65a4a85634
lnesh <107777140+lnesh@users.noreply.github.com>
2023-04-22 16:05:21 +0300

    Added Bottom Navbar
    Also some small RecyclerView changes

--- app/src/main/java/com/mobile/physiolink/ui/DoctorActivity.java
+++ app/src/main/java/com/mobile/physiolink/ui/DoctorActivity.java
@@ -1,13 +1,28 @@
 package com.mobile.physiolink.ui;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+import com.mobile.physiolink.R;
 import com.mobile.physiolink.databinding.ActivityDoctorBinding;
+import com.mobile.physiolink.ui.doctor.Appointments_Fragment;
+import com.mobile.physiolink.ui.doctor.DoctorHomeFragment;
+import com.mobile.physiolink.ui.doctor.DoctorPatientsFragment;
+import com.mobile.physiolink.ui.doctor.DoctorServicesFragment;
 
 public class DoctorActivity extends AppCompatActivity
 {
+    BottomNavigationView bottomNavigationView;
+    DoctorHomeFragment doctorHomeFragment = new DoctorHomeFragment();
+    Appointments_Fragment doctorAppointmentsFragment = new Appointments_Fragment();
+    DoctorServicesFragment doctorServicesFragment = new DoctorServicesFragment();
+    DoctorPatientsFragment doctorPatientsFragment = new DoctorPatientsFragment();
+
     private ActivityDoctorBinding binding;
 
     @Override
@@ -16,5 +31,31 @@ public class DoctorActivity extends AppCompatActivity
         super.onCreate(savedInstanceBundle);
         binding = ActivityDoctorBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        bottomNavigationView = findViewById( R.id.bottom_navigation);
+        getSupportFragmentManager().beginTransaction().replace(R.id.container,doctorHomeFragment).commit();
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.home:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,doctorHomeFragment).commit();
+                        return true;
+                    case R.id.appointment:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,doctorAppointmentsFragment).commit();
+                        return true;
+                    case R.id.services:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,doctorServicesFragment).commit();
+                        return true;
+                    case R.id.patients:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,doctorPatientsFragment).commit();
+                        return true;
+                }
+
+                return false;
+            }
+        });
+
     }
 }
