c2a3fb27d2f5670f6adbefcc4afe1cafb706aeea
setokk <tesmtracks@gmail.com>
2023-04-22 16:45:59 +0300

    Fixed resource linking error issue for bottom_nav

--- app/src/main/java/com/mobile/physiolink/ui/DoctorActivity.java
+++ app/src/main/java/com/mobile/physiolink/ui/DoctorActivity.java
@@ -1,13 +1,9 @@
 package com.mobile.physiolink.ui;
 
 import android.os.Bundle;
-import android.view.MenuItem;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.navigation.NavigationBarView;
 import com.mobile.physiolink.R;
 import com.mobile.physiolink.databinding.ActivityDoctorBinding;
 import com.mobile.physiolink.ui.doctor.Appointments_Fragment;
@@ -17,12 +13,6 @@ import com.mobile.physiolink.ui.doctor.DoctorServicesFragment;
 
 public class DoctorActivity extends AppCompatActivity
 {
-    BottomNavigationView bottomNavigationView;
-    DoctorHomeFragment doctorHomeFragment = new DoctorHomeFragment();
-    Appointments_Fragment doctorAppointmentsFragment = new Appointments_Fragment();
-    DoctorServicesFragment doctorServicesFragment = new DoctorServicesFragment();
-    DoctorPatientsFragment doctorPatientsFragment = new DoctorPatientsFragment();
-
     private ActivityDoctorBinding binding;
 
     @Override
@@ -32,30 +22,25 @@ public class DoctorActivity extends AppCompatActivity
         binding = ActivityDoctorBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        bottomNavigationView = findViewById( R.id.bottom_navigation);
-        getSupportFragmentManager().beginTransaction().replace(R.id.container,doctorHomeFragment).commit();
-
-        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(MenuItem item) {
-                switch (item.getItemId()){
-                    case R.id.home:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.container,doctorHomeFragment).commit();
-                        return true;
-                    case R.id.appointment:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.container,doctorAppointmentsFragment).commit();
-                        return true;
-                    case R.id.services:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.container,doctorServicesFragment).commit();
-                        return true;
-                    case R.id.patients:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.container,doctorPatientsFragment).commit();
-                        return true;
-                }
-
-                return false;
+        binding.bottomNavigation.setOnItemSelectedListener(item ->
+        {
+            switch (item.getItemId())
+            {
+                case R.id.home:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, new DoctorHomeFragment()).commit();
+                    return true;
+                case R.id.appointment:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, new Appointments_Fragment()).commit();
+                    return true;
+                case R.id.services:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, new DoctorServicesFragment()).commit();
+                    return true;
+                case R.id.patients:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, new DoctorPatientsFragment()).commit();
+                    return true;
             }
-        });
 
+            return false;
+        });
     }
 }
