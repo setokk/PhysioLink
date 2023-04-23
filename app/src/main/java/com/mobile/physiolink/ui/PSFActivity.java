5d8ac97b63fa97067116f4c7cca541fa263dc0ce
setokk <tesmtracks@gmail.com>
2023-04-23 22:29:30 +0300

    Changed PsfHomeFragment listeners to different lifecycle

--- app/src/main/java/com/mobile/physiolink/ui/PSFActivity.java
+++ app/src/main/java/com/mobile/physiolink/ui/PSFActivity.java
@@ -3,16 +3,11 @@ package com.mobile.physiolink.ui;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentTransaction;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.mobile.physiolink.databinding.ActivityPsfBinding;
 
-import androidx.navigation.fragment.NavHostFragment;
-import androidx.navigation.ui.NavigationUI;
 import com.mobile.physiolink.R;
+import com.mobile.physiolink.ui.psf.CreateParoxesFragment;
 import com.mobile.physiolink.ui.psf.PsfHomeFragment;
 
 public class PSFActivity extends AppCompatActivity
@@ -20,6 +15,7 @@ public class PSFActivity extends AppCompatActivity
     private ActivityPsfBinding binding;
 
     private final PsfHomeFragment psfHomeFragment = new PsfHomeFragment();
+    private final CreateParoxesFragment createParoxesFragment = new CreateParoxesFragment();
 
     @Override
     public void onCreate(Bundle savedInstanceBundle)
@@ -27,22 +23,27 @@ public class PSFActivity extends AppCompatActivity
         super.onCreate(savedInstanceBundle);
         binding = ActivityPsfBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-//
-//
-//        getSupportFragmentManager().beginTransaction()
-//                .replace(R.id.fragmentContainerView,  new PsfHomeFragment())
-//                .commit();
 
-//        binding.bottomNavPsf.setOnItemSelectedListener(item ->
-//        {
-//            switch (item.getItemId())
-//            {
-//                case R.id.home_psf:
-//                    getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainerView, psfHomeFragment).commit();
-//                    return true;
-//            }
-//
-//            return false;
-//        });
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragmentContainerView, psfHomeFragment)
+                .commit();
+
+        /* Bottom Navigation Listener */
+        binding.bottomNavPsf.setOnItemSelectedListener(item ->
+        {
+            switch (item.getItemId())
+            {
+                case R.id.home_psf:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainerView, psfHomeFragment).commit();
+                    return true;
+                case R.id.profile_psf:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainerView, createParoxesFragment).commit();
+                    return true;
+
+            }
+
+           return false;
+        });
     }
 }
--- app/src/main/java/com/mobile/physiolink/ui/psf/PsfHomeFragment.java
+++ app/src/main/java/com/mobile/physiolink/ui/psf/PsfHomeFragment.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
@@ -29,9 +30,17 @@ public class PsfHomeFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+                             Bundle savedInstanceState)
+    {
         // Inflate the layout for this fragment
         binding = FragmentPsfHomeBinding.inflate(inflater, container, false);;
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState)
+    {
+        super.onViewCreated(view, savedInstanceState);
 
         //phisiotherapeftiria button
         binding.psfImageButton1.setOnClickListener((v)->{Navigation.findNavController(binding.getRoot()).navigate(R.id.action_fragment_home_psf_to_phisiotherpeftiriaFragment);});
@@ -39,9 +48,5 @@ public class PsfHomeFragment extends Fragment {
         //parohes button
         binding.psfImageButton2.setOnClickListener((v)->{Navigation.findNavController(binding.getRoot()).navigate(R.id.action_fragment_home_psf_to_parohesFragment);});
 
-        return binding.getRoot();
     }
-
-
-
 }
