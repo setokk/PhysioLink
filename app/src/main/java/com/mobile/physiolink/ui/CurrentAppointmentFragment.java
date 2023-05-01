23eed03aaf45120803d934e1e9d5bc184c62a743
christsarcho <ics21072@uom.edu.gr>
2023-05-01 20:59:38 +0300

    rename current appointment

--- app/src/main/java/com/mobile/physiolink/ui/CurrentAppointmentFragment.java
+++ app/src/main/java/com/mobile/physiolink/ui/CurrentAppointmentFragment.java
@@ -1,4 +1,47 @@
 package com.mobile.physiolink.ui;
 
-public class CurrentAppointmentFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.mobile.physiolink.databinding.FragmentCurrentAppointmentBinding;
+
+;
+
+
+public class CurrentAppointmentFragment extends Fragment
+{
+    private FragmentCurrentAppointmentBinding binding;
+
+    public CurrentAppointmentFragment()
+    {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState)
+    {
+        // Inflate the layout for this fragment
+        binding = FragmentCurrentAppointmentBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view,
+                              @Nullable Bundle savedInstanceState)
+    {
+        super.onViewCreated(view, savedInstanceState);
+    }
 }
