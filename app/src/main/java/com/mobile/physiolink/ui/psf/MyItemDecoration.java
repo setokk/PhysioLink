c0a3a47686d7c76e280edab03fe84e37f7a7a226
Viktoria Limenidou <viktorialimenidou@gmail.com>
2023-05-02 20:38:32 +0300

    Extra spacing to the last item of the RecyclerView in order to resolve a visibility issue

--- app/src/main/java/com/mobile/physiolink/ui/psf/MyItemDecoration.java
+++ app/src/main/java/com/mobile/physiolink/ui/psf/MyItemDecoration.java
@@ -17,8 +17,16 @@ public class MyItemDecoration extends RecyclerView.ItemDecoration {
     public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
         super.getItemOffsets(outRect, view, parent, state);
 
-        if (parent.getChildAdapterPosition(view) != parent.getAdapter().getItemCount() - 1) {
+        int itemPosition = parent.getChildAdapterPosition(view);
+        int itemCount = parent.getAdapter().getItemCount();
+
+        // add spacing to all items except the last one
+        if (itemPosition < itemCount - 1) {
             outRect.bottom = spacing;
+        }
+        // add extra spacing only to the last item
+        else {
+            outRect.bottom = spacing * 25; // adjust the spacing as needed
         }
     }
 }
