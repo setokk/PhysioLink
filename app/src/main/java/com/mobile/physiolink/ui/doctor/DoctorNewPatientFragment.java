034714c4bd4ca82122f40de9f90bdcf7a3997ed3
setokk <tesmtracks@gmail.com>
2023-05-04 11:39:43 +0300

    Added binding property to new patient fragment

--- app/src/main/java/com/mobile/physiolink/ui/doctor/DoctorNewPatientFragment.java
+++ app/src/main/java/com/mobile/physiolink/ui/doctor/DoctorNewPatientFragment.java
@@ -2,6 +2,8 @@ package com.mobile.physiolink.ui.doctor;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -9,26 +11,35 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.mobile.physiolink.R;
+import com.mobile.physiolink.databinding.FragmentDoctorNewPatientBinding;
 
 public class DoctorNewPatientFragment extends Fragment
 {
+    private FragmentDoctorNewPatientBinding binding;
+
     public DoctorNewPatientFragment()
     {
         // Required empty public constructor
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) 
+    public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+                             Bundle savedInstanceState)
+    {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_doctor_new_patient, container, false);
+        binding = FragmentDoctorNewPatientBinding.inflate(inflater, container, false);
+        return binding.getRoot();
     }
 
-
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState)
+    {
+        super.onViewCreated(view, savedInstanceState);
+    }
 }
