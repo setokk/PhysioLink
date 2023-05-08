9887978ce8a151757c35390820e5dc6e4f23721b
setokk <tesmtracks@gmail.com>
2023-05-08 21:58:00 +0300

    Removed more files to solve conflicts

---
+++ app/src/main/java/com/mobile/physiolink/model/validator/LoginInputValidator.java
@@ -0,0 +1,36 @@
+package com.mobile.physiolink.model.validator;
+
+import com.mobile.physiolink.model.user.User;
+
+import java.util.Optional;
+
+/**
+ * @author Kote Kostandin (setokk) <br>
+ * <a href="https://www.linkedin.com/in/kostandin-kote-255382223/">LinkedIn</a>
+ * <br>
+ * This class is used for the credentials validation from the {@link com.mobile.physiolink.LoginActivity}
+ * class.
+ */
+public class LoginInputValidator
+{
+    public static final String NOT_VALID = "NOT VALID";
+
+    /**
+     * This method is used for validating the credentials that a user entered
+     * and then returning a <b>VALID</b> or <b>NOT_VALID</b> user
+     * @param username the username input
+     * @param password the password input
+     * @return {@link com.mobile.physiolink.model.user.User}
+     */
+    public static User validateUser(String username,
+                                    String password)
+    {
+        /* Fetch from API */
+        // User user = ...
+
+        User user = new User(username, "Patient");
+
+        return Optional.ofNullable(user)
+                .orElse(new User(NOT_VALID));
+    }
+}
