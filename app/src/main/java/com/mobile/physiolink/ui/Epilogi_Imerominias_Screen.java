772d0cbe5a10b89c0faa5ee5ea9bb3e24c746ba0
Viktoria Limenidou <viktorialimenidou@gmail.com>
2023-05-18 21:06:54 +0300

    Celect date screen changes

--- app/src/main/java/com/mobile/physiolink/ui/Epilogi_Imerominias_Screen.java
+++ app/src/main/java/com/mobile/physiolink/ui/Epilogi_Imerominias_Screen.java
@@ -19,87 +19,39 @@ import com.mobile.physiolink.databinding.FragmentEpilogiImerominiasScreenBinding
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Epilogi_Imerominias_Screen#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class Epilogi_Imerominias_Screen extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    CalendarView calendarView;
+    TextView myDate;
     private FragmentEpilogiImerominiasScreenBinding binding;
 
     public Epilogi_Imerominias_Screen() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Epilogi_Imerominias_Screen.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Epilogi_Imerominias_Screen newInstance(String param1, String param2) {
-        Epilogi_Imerominias_Screen fragment = new Epilogi_Imerominias_Screen();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-
-
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentEpilogiImerominiasScreenBinding.inflate(inflater, container, false);
+        View view = binding.getRoot();
 
-        return binding.getRoot();
-    }
+        calendarView = view.findViewById(R.id.calendarView);
+        myDate = view.findViewById(R.id.dateText);
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-//        binding.datePicker.setOnClickListener(v -> {
-//            MaterialDatePicker datePicker = MaterialDatePicker.Builder.datePicker()
-//                            .setTitleText("Select date")
-//                            .setSelection(MaterialDatePicker.todayInUtcMilliseconds())
-//                            .build();
-//            datePicker.show(getActivity().getSupportFragmentManager(), " ");
-//        });
-        CalendarView v = binding.calendarView;
-
-        v.setOnDateChangeListener((view1, year, month, dayOfMonth) -> {
-            SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
-            String selDate = sdf.format(new Date(dayOfMonth, month, year));
-            binding.datePicker.setText(selDate);
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                String date = (month + 1) + "/" + dayOfMonth + "/" + year;
+                myDate.setText(date);
+            }
         });
 
-        TextView date = (TextView) binding.datePicker;
-        String debug = "test";
-        binding.datePicker.setText(debug);
+        return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 }
