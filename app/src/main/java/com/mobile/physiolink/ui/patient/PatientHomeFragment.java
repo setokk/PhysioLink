f0d1f83b1253933395c903d1bfb34733301b3178
Theodora <katseisvilitheodora@gmail.com>
2023-05-19 02:06:29 +0300

    Added the appointment fragment, now the patient home screen will show different fragments depending on whether the patient has or has not an appointment.

--- app/src/main/java/com/mobile/physiolink/ui/patient/PatientHomeFragment.java
+++ app/src/main/java/com/mobile/physiolink/ui/patient/PatientHomeFragment.java
@@ -2,17 +2,25 @@ package com.mobile.physiolink.ui.patient;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.mobile.physiolink.R;
 import com.mobile.physiolink.databinding.FragmentPatientHomeBinding;
+
 
 public class PatientHomeFragment extends Fragment
 {
+    boolean hasAppointment=true;
+    Fragment AppointmentFragment = new UpcomingAppointmentFragment();
+    Fragment NoAppointmentFragment= new NoUpcomingAppointmentFragment();
     private FragmentPatientHomeBinding binding;
 
     public PatientHomeFragment() {
@@ -28,5 +36,25 @@ public class PatientHomeFragment extends Fragment
     {
         binding = FragmentPatientHomeBinding.inflate(inflater, container, false);
         return binding.getRoot();
+
+    }
+
+    public void onViewCreated(@NonNull View view,
+                              @Nullable Bundle savedInstanceState)
+    {
+        super.onViewCreated(view, savedInstanceState);
+
+        if(hasAppointment){
+            FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
+            transaction.replace(R.id.upcomingAppointmentFragmentContainer, AppointmentFragment);
+            transaction.commit();
+        }
+        else{
+            FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
+            transaction.replace(R.id.upcomingAppointmentFragmentContainer, NoAppointmentFragment);
+            transaction.commit();
+        }
+
     }
 }
+
