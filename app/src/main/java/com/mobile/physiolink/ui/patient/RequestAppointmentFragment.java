23f6a8b723676e9b5f1a043ba379fab84cce8871
Viktoria Limenidou <viktorialimenidou@gmail.com>
2023-05-19 19:17:15 +0300

    Drop Down Time List in RequestAppointment

--- app/src/main/java/com/mobile/physiolink/ui/patient/RequestAppointmentFragment.java
+++ app/src/main/java/com/mobile/physiolink/ui/patient/RequestAppointmentFragment.java
@@ -9,12 +9,21 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 import com.mobile.physiolink.databinding.FragmentRequestAppointmentBinding;
 import com.mobile.physiolink.util.DateFormatter;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class RequestAppointmentFragment extends Fragment
 {
+    private List<String> dropdownData;
+    private ArrayAdapter<String> adapter;
+
     private FragmentRequestAppointmentBinding binding;
 
     public RequestAppointmentFragment() {
@@ -25,6 +34,7 @@ public class RequestAppointmentFragment extends Fragment
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
         binding = FragmentRequestAppointmentBinding.inflate(inflater, container, false);
+
         return binding.getRoot();
     }
 
@@ -33,6 +43,18 @@ public class RequestAppointmentFragment extends Fragment
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState)
     {
+        dropdownData = Arrays.asList("15:00-16:00", "16:00-17:00", "17:00-18:00","18:00-19:00","19:00-20:00","20:00-21:00","22:00-23:00","23:00-24:00");
+        adapter = new ArrayAdapter<>(requireContext(), android.R.layout.simple_spinner_dropdown_item, dropdownData);
+        binding.autoCompleteTime.setAdapter(adapter);
+
+        // Handle item selection
+        binding.autoCompleteTime.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String selectedOption = adapter.getItem(position);
+                // Handle the selected option
+            }
+        });
         binding.calendarView.setOnDateChangeListener((view1, year, month, dayOfMonth) ->
         {
             String date = DateFormatter.formatToAlphanumeric(year, month + 1, dayOfMonth);
