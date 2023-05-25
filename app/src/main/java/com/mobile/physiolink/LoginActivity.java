1cc1f02c261e5c463bb933753cb0e0063d9570af
Panos-Stergioulas-Mpolis <panosstergioulasmpolis@gmail.com>
2023-05-25 21:16:58 +0300

    Bottom navbar is hidden when keyboard apears

--- app/src/main/java/com/mobile/physiolink/LoginActivity.java
+++ app/src/main/java/com/mobile/physiolink/LoginActivity.java
@@ -60,7 +60,7 @@ public class LoginActivity extends AppCompatActivity
 
         if (username.isEmpty() || password.isEmpty())
         {
-            startActivity(new Intent(this, DoctorActivity.class));
+            startActivity(new Intent(this, PSFActivity.class));
             Toast.makeText(this, "Δεν επιτρέπονται κενά πεδία!", Toast.LENGTH_SHORT).show();
             return;
         }
