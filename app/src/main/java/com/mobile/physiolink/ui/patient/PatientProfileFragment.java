ff7abac9afac778956ec6528ae7f2c783239fd5d
Viktoria Limenidou <viktorialimenidou@gmail.com>
2023-05-27 02:53:40 +0300

    Connection with data base in patient profile

--- app/src/main/java/com/mobile/physiolink/ui/patient/PatientProfileFragment.java
+++ app/src/main/java/com/mobile/physiolink/ui/patient/PatientProfileFragment.java
@@ -10,6 +10,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.mobile.physiolink.databinding.FragmentPatientProfileBinding;
+import com.mobile.physiolink.model.user.singleton.UserHolder;
 
 public class PatientProfileFragment extends Fragment{
     private FragmentPatientProfileBinding binding;
@@ -31,6 +32,22 @@ public class PatientProfileFragment extends Fragment{
     {
         // Inflate the layout for this fragment
         binding = FragmentPatientProfileBinding.inflate(inflater, container, false);
+        binding.profileNamePatient.setText(String.format("%s %s",
+                UserHolder.patient().getName(), UserHolder.patient().getSurname()));
+        binding.profileUsernamePatient.setText(String.format("%s ",
+                UserHolder.patient().getUsername()));
+        binding.amkaOutputProfilePatient.setText(String.format("%s ",
+                UserHolder.patient().getAmka()));
+        binding.emailOutputPatientProfile.setText(String.format("%s ",
+                UserHolder.patient().getEmail()));
+        binding.cityOutputPatientProfile.setText(String.format("%s ",
+                UserHolder.patient().getCity()));
+        binding.adressOutputPatientProfile.setText(String.format("%s ",
+                UserHolder.patient().getAddress()));
+        binding.phoneOutputPatientProfile.setText(String.format("%s ",
+                UserHolder.patient().getPhoneNumber()));
+        binding.postalCodePatientProfile.setText(String.format("%s ",
+                UserHolder.patient().getPostalCode()));
         return binding.getRoot();
     }
 
