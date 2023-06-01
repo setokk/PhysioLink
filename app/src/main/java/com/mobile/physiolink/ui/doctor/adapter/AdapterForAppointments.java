a84c6e38d459496da224ca30b69a7448b92ffbca
AlexandraKoudouni <alexandra.koudouni@gmail.com>
2023-06-01 03:55:50 +0300

    initial phase

--- app/src/main/java/com/mobile/physiolink/ui/doctor/adapter/AdapterForAppointments.java
+++ app/src/main/java/com/mobile/physiolink/ui/doctor/adapter/AdapterForAppointments.java
@@ -3,6 +3,7 @@ package com.mobile.physiolink.ui.doctor.adapter;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -10,12 +11,27 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.mobile.physiolink.R;
 import com.mobile.physiolink.databinding.ItemDoctorAppointmentBinding;
 import com.mobile.physiolink.model.appointment.Appointment;
+import com.mobile.physiolink.model.user.singleton.UserHolder;
+import com.mobile.physiolink.service.api.API;
+import com.mobile.physiolink.service.api.RequestFacade;
+import com.mobile.physiolink.ui.doctor.OnButtonClickListener;
+import com.mobile.physiolink.ui.popup.AppointmentDeletePopUp;
+import com.mobile.physiolink.ui.popup.AppointmentRejectPopUp;
+import com.mobile.physiolink.ui.popup.EndOfAppointmentPopUp;
 import com.mobile.physiolink.util.date.TimeFormatter;
 
+import java.io.IOException;
 import java.util.Arrays;
+import java.util.HashMap;
 
-public class AdapterForAppointments extends RecyclerView.Adapter <AdapterForAppointments.MyViewHolder>
-{
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
+
+public class AdapterForAppointments extends RecyclerView.Adapter <AdapterForAppointments.MyViewHolder> implements OnButtonClickListener {
+    public static final int ACCEPT=1;
+    public static final int REJECT=2;
+
     private Appointment[] appointments;
 
     private boolean[] isExpanded;
@@ -39,7 +55,7 @@ public class AdapterForAppointments extends RecyclerView.Adapter <AdapterForAppo
     public AdapterForAppointments.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         ItemDoctorAppointmentBinding binding = ItemDoctorAppointmentBinding
                 .inflate(LayoutInflater.from(parent.getContext()), parent, false);
-        return new AdapterForAppointments.MyViewHolder(binding);
+        return new AdapterForAppointments.MyViewHolder(binding, this);
     }
 
     @Override
@@ -80,22 +96,55 @@ public class AdapterForAppointments extends RecyclerView.Adapter <AdapterForAppo
         notifyItemChanged(position);
     }
 
+    @Override
+    public void onButtonClicked(int position, int buttonId) {
+
+    }
 
 
     public class MyViewHolder extends RecyclerView.ViewHolder
     {
         ItemDoctorAppointmentBinding binding;
 
-
-
-        public MyViewHolder(ItemDoctorAppointmentBinding binding)
-        {
+        public MyViewHolder(ItemDoctorAppointmentBinding binding, OnButtonClickListener listener) {
             super(binding.getRoot());
             this.binding = binding;
 
             // Set click listener on the entire item view
             binding.getRoot().setOnClickListener(view ->
-                    toggleExpansion(getBindingAdapterPosition()));
+            {
+                    toggleExpansion(getBindingAdapterPosition());
+            });
+
+
+            binding.tickAppointmentButton.setOnClickListener(view ->
+            {
+                EndOfAppointmentPopUp paymentPopUp = new EndOfAppointmentPopUp(binding.patientImageDoctorAppointment, binding.appointmentTimeDoctorPatient, binding.appointmentNameDoctorPatient);
+                paymentPopUp.setPositiveOnClick((dialog, which) ->
+                {
+
+                });
+
+                paymentPopUp.setNegativeOnClick((dialog, which) ->
+                {
+
+                });
+
+            });
+
+            binding.exAppointmentButton.setOnClickListener(view ->
+            {
+                AppointmentDeletePopUp deletePopUp = new AppointmentDeletePopUp("Διαγραφή Ραντεβού", "Είστε σίγουρος οτι θέλετε να διαγράψετε το ραντεβού;",
+                        "Ναι", "Όχι");
+                deletePopUp.setPositiveOnClick((dialog, which) ->
+                {
+
+                });
+                deletePopUp.setNegativeOnClick((dialog, which) ->
+                {
+
+                });
+            });
         }
     }
 }
