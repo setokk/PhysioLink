26b205b82797e35371cf4fba6d5728d71343c34f
Viktoria Limenidou <viktorialimenidou@gmail.com>
2023-06-02 19:51:39 +0300

    Settings added doc,pat,psf and navigations

---
+++ app/src/main/java/com/mobile/physiolink/ui/doctor/DoctorSettingsFragment.java
@@ -0,0 +1,44 @@
+package com.mobile.physiolink.ui.doctor;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.mobile.physiolink.databinding.FragmentDoctorSettingsBinding;
+
+
+public class DoctorSettingsFragment extends Fragment {
+    private FragmentDoctorSettingsBinding binding;
+
+    public DoctorSettingsFragment() {}
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState)
+    {
+        // Inflate the layout for this fragment
+        binding = FragmentDoctorSettingsBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState)
+    {
+        super.onViewCreated(view, savedInstanceState);
+
+
+
+    }
+}
+
---
+++ app/src/main/java/com/mobile/physiolink/ui/patient/PatientSettingsFragment.java
@@ -0,0 +1,43 @@
+package com.mobile.physiolink.ui.patient;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.mobile.physiolink.databinding.FragmentPatientSettingsBinding;
+
+public class PatientSettingsFragment extends Fragment {
+    private FragmentPatientSettingsBinding binding;
+
+    public PatientSettingsFragment() {}
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState)
+    {
+        // Inflate the layout for this fragment
+        binding = FragmentPatientSettingsBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState)
+    {
+        super.onViewCreated(view, savedInstanceState);
+
+
+
+    }
+}
+
