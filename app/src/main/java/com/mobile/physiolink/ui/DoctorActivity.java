c41987161c4c27aca6d713b6ded5fcb61cd9a032
setokk <tesmtracks@gmail.com>
2023-06-03 23:05:21 +0300

    merge conflict resolve 5

--- app/src/main/java/com/mobile/physiolink/ui/DoctorActivity.java
+++ app/src/main/java/com/mobile/physiolink/ui/DoctorActivity.java
@@ -3,21 +3,18 @@ package com.mobile.physiolink.ui;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
-import androidx.navigation.NavDestination;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.mobile.physiolink.R;
 import com.mobile.physiolink.databinding.ActivityDoctorBinding;
-import com.mobile.physiolink.service.notification.NotificationService;
 
 public class DoctorActivity extends AppCompatActivity
 {
@@ -31,6 +28,7 @@ public class DoctorActivity extends AppCompatActivity
         binding = ActivityDoctorBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+
         /* Setup Navigation with top level destinations */
         NavController navController = Navigation.findNavController(this, R.id.container);
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
@@ -42,6 +40,39 @@ public class DoctorActivity extends AppCompatActivity
         configureNotificationChannel();
         Intent notificationService = new Intent(getApplicationContext(), NotificationService.class);
         startService(notificationService);
+
+
+        /*ActionBar*/
+        getSupportActionBar().setTitle("PhysioLink");
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.drawable.physiolink_logo);
+
+
+    }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.doc_top, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.docSettings) {
+            NavController navController = Navigation.findNavController(this, R.id.container);
+            navController.navigate(R.id.doctorSettingsFragment);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
     }
 
     @Override
